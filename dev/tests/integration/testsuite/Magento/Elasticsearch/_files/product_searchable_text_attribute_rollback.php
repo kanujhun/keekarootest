5ad399dcbaf92ce759f4468d559b171ede4c3212
EYStudios <info@eystudios.com>
2018-01-04 09:51:33 +0000

    Upgrade 2.1.10

---
+++ dev/tests/integration/testsuite/Magento/Elasticsearch/_files/product_searchable_text_attribute_rollback.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+require __DIR__ . '/../../Catalog/_files/products_rollback.php';
+
+$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+$registry = $objectManager->get(\Magento\Framework\Registry::class);
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+/** @var $attribute \Magento\Catalog\Model\ResourceModel\Eav\Attribute */
+$attribute = $objectManager->create(
+    \Magento\Catalog\Model\ResourceModel\Eav\Attribute::class
+);
+$attribute->load('text_attribute', 'attribute_code');
+if ($attribute->getId()) {
+    $attribute->delete();
+}
+
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', false);
+/** @var \Magento\Eav\Model\Config $eavConfig */
+$eavConfig = $objectManager->get(\Magento\Eav\Model\Config::class);
+$eavConfig->clear();
